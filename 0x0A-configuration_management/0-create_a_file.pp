57659e4e883e790175ef226c41c43899be8dbc9b
charlespaul1 <charlespaulwabomba@gmail.com>
2023-01-11 19:52:24 +0300

    Using Puppet, create a file in /tmp

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+# creating a file in tmp directory
+
+file {'school':
+    ensure  => 'present',
+    content => 'I love Puppet',
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
+    path    => '/tmp/school',
+}
